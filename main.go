b6d1b4c20710bf7ef1d99642cce43ec9116551e2
Jared Warren <jlwarren1@gmail.com>
2024-09-20 19:59:42 -0600

    test

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
-import (
-	// "github.com/jgarff/rpi_ws281x/golang/ws2811"
-	ws2811 "github.com/rpi-ws281x/rpi-ws281x-go"
-)
+import "fmt"
+
+// "github.com/jgarff/rpi_ws281x/golang/ws2811"
 
 const (
 	brightness = 128
@@ -19,27 +18,5 @@ func checkError(err error) {
 }
 
 func main() {
-	opt := ws2811.DefaultOptions
-	opt.Channels[0].Brightness = brightness
-	opt.Channels[0].LedCount = ledCounts
-
-	dev, err := ws2811.MakeWS2811(&opt)
-	checkError(err)
-
-	checkError(dev.Init())
-	defer dev.Fini()
-
-	for x := 0; x < width; x++ {
-		for y := 0; y < height; y++ {
-			color := uint32(0xff0000)
-			if x > 2 && x < 5 && y > 0 && y < 7 {
-				color = 0xffffff
-			}
-			if x > 0 && x < 7 && y > 2 && y < 5 {
-				color = 0xffffff
-			}
-			dev.Leds(0)[x*height+y] = color
-		}
-	}
-	checkError(dev.Render())
+	fmt.Println(".......")
 }
