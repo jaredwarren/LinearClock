62e6eea3af5d56435e061d3121524c7e9004de71
Jared Warren <jlwarren1@gmail.com>
2024-09-20 19:44:40 -0600

    init

---
+++ main.go
@@ -0,0 +1,41 @@
+// Connects to an WS2812 RGB LED strip with 10 LEDS.
+//
+// See either the others.go or digispark.go files in this directory
+// for the neopixels pin assignments.
+package main
+
+import (
+	"image/color"
+	"machine"
+	"time"
+
+	"tinygo.org/x/drivers/ws2812"
+)
+
+var (
+	neo  machine.Pin
+	leds [10]color.RGBA
+)
+
+func main() {
+	neo.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+	ws := ws2812.NewWS2812(neo)
+	rg := false
+
+	for {
+		rg = !rg
+		for i := range leds {
+			rg = !rg
+			if rg {
+				// Alpha channel is not supported by WS2812 so we leave it out
+				leds[i] = color.RGBA{R: 0xff, G: 0x00, B: 0x00}
+			} else {
+				leds[i] = color.RGBA{R: 0x00, G: 0xff, B: 0x00}
+			}
+		}
+
+		ws.WriteColors(leds[:])
+		time.Sleep(100 * time.Millisecond)
+	}
+}
